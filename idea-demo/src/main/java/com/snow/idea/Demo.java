8b64ace7f122a4995c73c77fb66074f9ed54a922
jiaxue.yu <197638651@qq.com>
2023-12-14 08:33:18 +0800

    feat[project:idea]:init

---
+++ idea-demo/src/main/java/com/snow/idea/Demo.java
@@ -0,0 +1,12 @@
+package com.snow.idea;
+
+/**
+ * @author jiaXue
+ * date 2023/12/14
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("demo 1111");
+    }
+}
