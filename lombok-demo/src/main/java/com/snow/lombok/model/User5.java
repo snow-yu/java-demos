9cced1048cb202f140373679b4174e27005e4a0a
jiaxue.yu <197638651@qq.com>
2023-12-25 07:55:48 +0800

    doc[module:lomobk]:注释完善

--- lombok-demo/src/main/java/com/snow/lombok/model/User5.java
+++ lombok-demo/src/main/java/com/snow/lombok/model/User5.java
@@ -6,7 +6,8 @@ import lombok.Value;
 /**
  * @author jiaXue
  * date 2023/12/20
- * @Value 注解添加后，等于同时添加以下注解; 主要用于某个类的值不希望被更改，只读
+ * @Value 注解添加后，等于同时添加以下注解; 主要用于某个类的值不希望被更改，只
+ * 自动给类的属性添加final方法，并且提供构造方法，可以不用写构造方法
  * @Getter
  * @ToString
  * @EqualsAndHashCode
